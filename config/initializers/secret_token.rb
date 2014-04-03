326ec7d45b7ce9125df1d3d89ba75fee6472fccd
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-03 11:41:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MillertimeApp::Application.config.secret_key_base = 'f87cc85b603ff5c813e2b49b81195408e1e87a3f99f098073dbc88767b3ea6624da69fa2aaa5c9b8eb76ec758a24fe03c95a12954a98c01909bbbebb8bfe8c48'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MillertimeApp::Application.config.session_store :cookie_store, key: '_millertime_app_session'
